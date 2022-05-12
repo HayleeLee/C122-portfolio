9f2043b3899e526395652d048661ddbcda661bb8
HayleeLee <98601945+HayleeLee@users.noreply.github.com>
2022-05-12 11:08:03 -0400

    Add files via upload

---
+++ Dictionary-it-3 복사본/script.js
@@ -0,0 +1,65 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
+image_array = [
+  'a.png',
+   'b.PNG' 
+]
+btn.addEventListener("click", () => {
+
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                
+
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p><br><br><br><div class="border-1"></div><br><br><br>
+
+            <div class="word-1">
+                    <h3 class="english">${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning-1">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                
+
+                <p class="word-example-1">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>
+`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+});
+function playSound() {
+    sound.play();
+}
+
